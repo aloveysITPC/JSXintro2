f70c15c41e115bd4702b2522dc3c773d06692924
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-14 21:07:35 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// var React = require("react");  can instead use new key word "import"
+import React from "react";
+// var ReactDOM = require("react-dom");
+import ReactDOM from "react-dom";
+
+// ReactDOM.render(What to Show, Where to Show it )
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
